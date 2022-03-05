d59ad321fd3965f85b4c862bef5fb3d4e648ba42
Inzaghi <inzaghi.moniaga@gmail.com>
2022-03-05 11:51:48 -0800

    Test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
- * Template Name: Reveal - v4.7.0
- * Template URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 (function () {
   "use strict";
 
@@ -278,42 +272,12 @@
       clickable: true,
     },
   });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-    },
-  });
-
   /**
    * Animation on scroll
    */
   window.addEventListener("load", () => {
     AOS.init({
-      duration: 1000,
+      duration: 300,
       easing: "ease-in-out",
       once: true,
       mirror: false,
