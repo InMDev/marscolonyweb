ad8fc46f0fbe1d15e48efffe9ea139c7f4fa1ce6
Inzaghi <inzaghi.moniaga@gmail.com>
2022-03-05 15:11:27 -0800

    Animation Gone???

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,7 +277,7 @@
    */
   window.addEventListener("load", () => {
     AOS.init({
-      duration: 300,
+      duration: 1000,
       easing: "ease-in-out",
       once: true,
       mirror: false,
