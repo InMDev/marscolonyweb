bd43ef43eea30975b630097294fbf02d5caaac7d
InMDev <inzaghi.moniaga@gmail.com>
2022-03-19 13:04:55 -0700

    19 march

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,19 +273,4 @@ AOS.init();
       clickable: true,
     },
   });
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
-  });
 })();
-
-AOS.init({
-  duration: 1000
-});
