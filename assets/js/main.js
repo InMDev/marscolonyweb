b9def455515b9b5ac16c0b1f43b83eb42db7fe8c
InMDev <inzaghi.moniaga@gmail.com>
2022-05-14 14:15:47 -0700

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,21 +120,6 @@ function turnleft(){
     onscroll(document, headerFixed);
   }
 
-  /**
-   * Back to top button
-   */
-  let backtotop = select(".back-to-top");
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add("active");
-      } else {
-        backtotop.classList.remove("active");
-      }
-    };
-    window.addEventListener("load", toggleBacktotop);
-    onscroll(document, toggleBacktotop);
-  }
 
   /**
    * Mobile nav toggle
